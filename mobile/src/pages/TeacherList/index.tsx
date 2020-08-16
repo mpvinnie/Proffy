a38527e6c4d290e3083ba2323317790ae28501e7
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-15 23:17:08 -0400

    Favorites

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView } from 'react-native'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
@@ -11,7 +11,19 @@ function TeacherList() {
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis"/>
 
-      <TeacherItem />
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   )
 }
